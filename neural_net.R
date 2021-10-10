b995093a3679aef49c5a3443a0f4c9a15738746b
oro-sean <oro.sean@gmail.com>
2021-10-10 18:02:20 +1100

    finish NN upto grid search

--- neural_net.R
+++ neural_net.R
@@ -9,50 +9,381 @@ library(keras, quietly = TRUE)
 library(ggplot2, quietly = TRUE)
 library(tensorflow, quietly = TRUE)
 library(reticulate, quietly = TRUE)
+library(caret, quietly = TRUE)
 
+Sys.setenv(RETICULATE_PYTHON = "/home/veering/anaconda3/envs/r-reticulate/bin/python")
+Sys.getenv("RETICULATE_PYTHON")
+py_config()
+start <- Sys.time()
 data <- readRDS(file = "data.RDS")
 results <- as.data.frame(data[5])
 trainAss <- as.data.frame(data[6])
 testPred <- as.matrix(as.data.frame(data[7]))
-testResponse <- unlist(data[8])
+testResponse <- as.numeric(unlist(data[8]))
 trainPred <- as.matrix(as.data.frame(data[9]))
-trainResponse <- unlist(data[10])
+trainResponse <- as.numeric(unlist(data[10]))
 
-conda_binary(conda = "auto")
-conda_python(conda = "auto")
-conda_list(conda = "auto")
+trainIndex <- createDataPartition(trainResponse, p = 0.8, times = 4, list = FALSE)
 
-install_keras(method = "conda", conda = "auto", envname = "r-reticulate")
-conda_install("r-reticulate", "tensorflow")
-
-Sys.getenv("RETICULATE_PYTHON")
-Sys.setenv(RETICULATE_PYTHON = "C:/Users/veering_windows/anaconda3/envs/r-reticulate/python.exe")
-
-model <- keras_model_sequential() %>%
-  layer_dense(units = 19, activation = "relu", input_shape = c(ncol(testPred))) %>%
-  layer_dense(unit = 19, activation = "relu") %>%
+model_2_4 <- keras_model_sequential() %>%
+  layer_dense(units = 4, activation = "relu", input_shape = c(ncol(testPred))) %>%
+  layer_dense(unit = 4, activation = "relu") %>%
+  layer_dense(unit = 4, activation = "relu") %>%
   layer_dense(units = 1)
 
-model %>% compile(
+model_2_8 <- keras_model_sequential() %>%
+  layer_dense(units = 8, activation = "relu", input_shape = c(ncol(testPred))) %>%
+  layer_dense(unit = 8, activation = "relu") %>%
+  layer_dense(unit = 8, activation = "relu") %>%
+  layer_dense(units = 1)
+
+model_2_16 <- keras_model_sequential() %>%
+  layer_dense(units = 16, activation = "relu", input_shape = c(ncol(testPred))) %>%
+  layer_dense(unit = 16, activation = "relu") %>%
+  layer_dense(unit = 16, activation = "relu") %>%
+  layer_dense(units = 1)
+
+model_2_32 <- keras_model_sequential() %>%
+  layer_dense(units = 32, activation = "relu", input_shape = c(ncol(testPred))) %>%
+  layer_dense(unit = 32, activation = "relu") %>%
+  layer_dense(unit = 32, activation = "relu") %>%
+  layer_dense(units = 1)
+
+model_4_4 <- keras_model_sequential() %>%
+  layer_dense(units = 4, activation = "relu", input_shape = c(ncol(testPred))) %>%
+  layer_dense(unit = 4, activation = "relu") %>%
+  layer_dense(unit = 4, activation = "relu") %>%
+  layer_dense(unit = 4, activation = "relu") %>%
+  layer_dense(unit = 4, activation = "relu") %>%
+  layer_dense(units = 1)
+
+model_4_8 <- keras_model_sequential() %>%
+  layer_dense(units = 8, activation = "relu", input_shape = c(ncol(testPred))) %>%
+  layer_dense(unit = 8, activation = "relu") %>%
+  layer_dense(unit = 8, activation = "relu") %>%
+  layer_dense(unit = 8, activation = "relu") %>%
+  layer_dense(unit = 8, activation = "relu") %>%
+  layer_dense(units = 1)
+
+model_4_16 <- keras_model_sequential() %>%
+  layer_dense(units = 16, activation = "relu", input_shape = c(ncol(testPred))) %>%
+  layer_dense(unit = 16, activation = "relu") %>%
+  layer_dense(unit = 16, activation = "relu") %>%
+  layer_dense(unit = 16, activation = "relu") %>%
+  layer_dense(unit = 16, activation = "relu") %>%
+  layer_dense(units = 1)
+
+model_4_32 <- keras_model_sequential() %>%
+  layer_dense(units = 32, activation = "relu", input_shape = c(ncol(testPred))) %>%
+  layer_dense(unit = 32, activation = "relu") %>%
+  layer_dense(unit = 32, activation = "relu") %>%
+  layer_dense(unit = 32, activation = "relu") %>%
+  layer_dense(unit = 32, activation = "relu") %>%
+  layer_dense(units = 1)
+
+model_8_4 <- keras_model_sequential() %>%
+  layer_dense(units = 4, activation = "relu", input_shape = c(ncol(testPred))) %>%
+  layer_dense(unit = 4, activation = "relu") %>%
+  layer_dense(unit = 4, activation = "relu") %>%
+  layer_dense(unit = 4, activation = "relu") %>%
+  layer_dense(unit = 4, activation = "relu") %>%
+  layer_dense(unit = 4, activation = "relu") %>%
+  layer_dense(unit = 4, activation = "relu") %>%
+  layer_dense(unit = 4, activation = "relu") %>%
+  layer_dense(unit = 4, activation = "relu") %>%
+  layer_dense(units = 1)
+
+model_8_8 <- keras_model_sequential() %>%
+  layer_dense(units = 8, activation = "relu", input_shape = c(ncol(testPred))) %>%
+  layer_dense(unit = 8, activation = "relu") %>%
+  layer_dense(unit = 8, activation = "relu") %>%
+  layer_dense(unit = 8, activation = "relu") %>%
+  layer_dense(unit = 8, activation = "relu") %>%
+  layer_dense(unit = 8, activation = "relu") %>%
+  layer_dense(unit = 8, activation = "relu") %>%
+  layer_dense(unit = 8, activation = "relu") %>%
+  layer_dense(unit = 8, activation = "relu") %>%
+  layer_dense(units = 1)
+
+model_8_16 <- keras_model_sequential() %>%
+  layer_dense(units = 16, activation = "relu", input_shape = c(ncol(testPred))) %>%
+  layer_dense(unit = 16, activation = "relu") %>%
+  layer_dense(unit = 16, activation = "relu") %>%
+  layer_dense(unit = 16, activation = "relu") %>%
+  layer_dense(unit = 16, activation = "relu") %>%
+  layer_dense(unit = 16, activation = "relu") %>%
+  layer_dense(unit = 16, activation = "relu") %>%
+  layer_dense(unit = 16, activation = "relu") %>%
+  layer_dense(unit = 16, activation = "relu") %>%
+  layer_dense(units = 1)
+
+model_8_32 <- keras_model_sequential() %>%
+  layer_dense(units = 32, activation = "relu", input_shape = c(ncol(testPred))) %>%
+  layer_dense(unit = 32, activation = "relu") %>%
+  layer_dense(unit = 32, activation = "relu") %>%
+  layer_dense(unit = 32, activation = "relu") %>%
+  layer_dense(unit = 32, activation = "relu") %>%
+  layer_dense(unit = 32, activation = "relu") %>%
+  layer_dense(unit = 32, activation = "relu") %>%
+  layer_dense(unit = 32, activation = "relu") %>%
+  layer_dense(unit = 32, activation = "relu") %>%
+  layer_dense(units = 1)
+
+model_16_4 <- keras_model_sequential() %>%
+  layer_dense(units = 4, activation = "relu", input_shape = c(ncol(testPred))) %>%
+  layer_dense(unit = 4, activation = "relu") %>%
+  layer_dense(unit = 4, activation = "relu") %>%
+  layer_dense(unit = 4, activation = "relu") %>%
+  layer_dense(unit = 4, activation = "relu") %>%
+  layer_dense(unit = 4, activation = "relu") %>%
+  layer_dense(unit = 4, activation = "relu") %>%
+  layer_dense(unit = 4, activation = "relu") %>%
+  layer_dense(unit = 4, activation = "relu") %>%
+  layer_dense(unit = 4, activation = "relu") %>%
+  layer_dense(unit = 4, activation = "relu") %>%
+  layer_dense(unit = 4, activation = "relu") %>%
+  layer_dense(unit = 4, activation = "relu") %>%
+  layer_dense(unit = 4, activation = "relu") %>%
+  layer_dense(unit = 4, activation = "relu") %>%
+  layer_dense(unit = 4, activation = "relu") %>%
+  layer_dense(unit = 4, activation = "relu") %>%
+  layer_dense(units = 1)
+
+model_16_8 <- keras_model_sequential() %>%
+  layer_dense(units = 8, activation = "relu", input_shape = c(ncol(testPred))) %>%
+  layer_dense(unit = 8, activation = "relu") %>%
+  layer_dense(unit = 8, activation = "relu") %>%
+  layer_dense(unit = 8, activation = "relu") %>%
+  layer_dense(unit = 8, activation = "relu") %>%
+  layer_dense(unit = 8, activation = "relu") %>%
+  layer_dense(unit = 8, activation = "relu") %>%
+  layer_dense(unit = 8, activation = "relu") %>%
+  layer_dense(unit = 8, activation = "relu") %>%
+  layer_dense(unit = 8, activation = "relu") %>%
+  layer_dense(unit = 8, activation = "relu") %>%
+  layer_dense(unit = 8, activation = "relu") %>%
+  layer_dense(unit = 8, activation = "relu") %>%
+  layer_dense(unit = 8, activation = "relu") %>%
+  layer_dense(unit = 8, activation = "relu") %>%
+  layer_dense(unit = 8, activation = "relu") %>%
+  layer_dense(unit = 8, activation = "relu") %>%
+  layer_dense(units = 1)
+
+model_16_16 <- keras_model_sequential() %>%
+  layer_dense(units = 16, activation = "relu", input_shape = c(ncol(testPred))) %>%
+  layer_dense(unit = 16, activation = "relu") %>%
+  layer_dense(unit = 16, activation = "relu") %>%
+  layer_dense(unit = 16, activation = "relu") %>%
+  layer_dense(unit = 16, activation = "relu") %>%
+  layer_dense(unit = 16, activation = "relu") %>%
+  layer_dense(unit = 16, activation = "relu") %>%
+  layer_dense(unit = 16, activation = "relu") %>%
+  layer_dense(unit = 16, activation = "relu") %>%
+  layer_dense(unit = 16, activation = "relu") %>%
+  layer_dense(unit = 16, activation = "relu") %>%
+  layer_dense(unit = 16, activation = "relu") %>%
+  layer_dense(unit = 16, activation = "relu") %>%
+  layer_dense(unit = 16, activation = "relu") %>%
+  layer_dense(unit = 16, activation = "relu") %>%
+  layer_dense(unit = 16, activation = "relu") %>%
+  layer_dense(unit = 16, activation = "relu") %>%
+  layer_dense(units = 1)
+
+model_16_32 <- keras_model_sequential() %>%
+  layer_dense(units = 32, activation = "relu", input_shape = c(ncol(testPred))) %>%
+  layer_dense(unit = 32, activation = "relu") %>%
+  layer_dense(unit = 32, activation = "relu") %>%
+  layer_dense(unit = 32, activation = "relu") %>%
+  layer_dense(unit = 32, activation = "relu") %>%
+  layer_dense(unit = 32, activation = "relu") %>%
+  layer_dense(unit = 32, activation = "relu") %>%
+  layer_dense(unit = 32, activation = "relu") %>%
+  layer_dense(unit = 32, activation = "relu") %>%
+  layer_dense(unit = 32, activation = "relu") %>%
+  layer_dense(unit = 32, activation = "relu") %>%
+  layer_dense(unit = 32, activation = "relu") %>%
+  layer_dense(unit = 32, activation = "relu") %>%
+  layer_dense(unit = 32, activation = "relu") %>%
+  layer_dense(unit = 32, activation = "relu") %>%
+  layer_dense(unit = 32, activation = "relu") %>%
+  layer_dense(unit = 32, activation = "relu") %>%
+  layer_dense(units = 1)
+
+model_2_4 %>% compile(
   optimizer = "adam",
   loss = "mse",
   metrics = c("mae")
 )
 
-ModelHistory <- model %>% fit(trainPred, trainResponse, epochs = 300, batch_size = 8, validation_split=1/3)
+model_2_8 %>% compile(
+  optimizer = "adam",
+  loss = "mse",
+  metrics = c("mae")
+)
+
+model_2_16 %>% compile(
+  optimizer = "adam",
+  loss = "mse",
+  metrics = c("mae")
+)
+
+model_2_32 %>% compile(
+  optimizer = "adam",
+  loss = "mse",
+  metrics = c("mae")
+)
+
+model_4_4 %>% compile(
+  optimizer = "adam",
+  loss = "mse",
+  metrics = c("mae")
+)
+
+model_4_8 %>% compile(
+  optimizer = "adam",
+  loss = "mse",
+  metrics = c("mae")
+)
+
+model_4_16 %>% compile(
+  optimizer = "adam",
+  loss = "mse",
+  metrics = c("mae")
+)
+
+model_4_32 %>% compile(
+  optimizer = "adam",
+  loss = "mse",
+  metrics = c("mae")
+)
+
+model_8_4 %>% compile(
+  optimizer = "adam",
+  loss = "mse",
+  metrics = c("mae")
+)
+
+model_8_8 %>% compile(
+  optimizer = "adam",
+  loss = "mse",
+  metrics = c("mae")
+)
+
+model_8_16 %>% compile(
+  optimizer = "adam",
+  loss = "mse",
+  metrics = c("mae")
+)
+
+model_8_32 %>% compile(
+  optimizer = "adam",
+  loss = "mse",
+  metrics = c("mae")
+)
+
+model_16_4 %>% compile(
+  optimizer = "adam",
+  loss = "mse",
+  metrics = c("mae")
+)
+
+model_16_8 %>% compile(
+  optimizer = "adam",
+  loss = "mse",
+  metrics = c("mae")
+)
+
+model_16_16 %>% compile(
+  optimizer = "adam",
+  loss = "mse",
+  metrics = c("mae")
+)
+
+model_16_32 %>% compile(
+  optimizer = "adam",
+  loss = "mse",
+  metrics = c("mae")
+)
 
 
+tensorboard("my_log_dir")
+callbacks = list(callback_tensorboard(log_dir = "my_log_dir",
+                                      histogram_freq = 1,
+                                      ),
+                 callback_early_stopping(
+                   monitor = "mae", patience = 5,
+                   min_delta = .005,
+                   mode = "min"
+  )
+)
 
+
+his <- c()
+for(b in c(2,4,8,16,32,64)){
+for( i in 1:4){
+ModelHistory <- model_2_4 %>% fit(trainPred[trainIndex[ ,i], ], trainResponse[trainIndex[, i]], epochs = 100, batch_size = b, validation_data = list(trainPred[-trainIndex[, i], ], trainResponse[-trainIndex[ ,i]]), callbacks = callbacks)
+his <- rbind(his, ModelHistory$metrics) 
+
+ModelHistory <- model_2_8 %>% fit(trainPred[trainIndex[ ,i], ], trainResponse[trainIndex[, i]], epochs = 100, batch_size = b, validation_data = list(trainPred[-trainIndex[, i], ], trainResponse[-trainIndex[ ,i]]), callbacks = callbacks)
+his <- rbind(his, ModelHistory$metrics) 
+
+ModelHistory <- model_2_16 %>% fit(trainPred[trainIndex[ ,i], ], trainResponse[trainIndex[, i]], epochs = 100, batch_size = b, validation_data = list(trainPred[-trainIndex[, i], ], trainResponse[-trainIndex[ ,i]]), callbacks = callbacks)
+his <- rbind(his, ModelHistory$metrics) 
+
+ModelHistory <- model_2_32 %>% fit(trainPred[trainIndex[ ,i], ], trainResponse[trainIndex[, i]], epochs = 100, batch_size = b, validation_data = list(trainPred[-trainIndex[, i], ], trainResponse[-trainIndex[ ,i]]), callbacks = callbacks)
+his <- rbind(his, ModelHistory$metrics) 
+
+ModelHistory <- model_4_4 %>% fit(trainPred[trainIndex[ ,i], ], trainResponse[trainIndex[, i]], epochs = 100, batch_size = b, validation_data = list(trainPred[-trainIndex[, i], ], trainResponse[-trainIndex[ ,i]]), callbacks = callbacks)
+his <- rbind(his, ModelHistory$metrics) 
+
+ModelHistory <- model_4_8 %>% fit(trainPred[trainIndex[ ,i], ], trainResponse[trainIndex[, i]], epochs = 100, batch_size = b, validation_data = list(trainPred[-trainIndex[, i], ], trainResponse[-trainIndex[ ,i]]), callbacks = callbacks)
+his <- rbind(his, ModelHistory$metrics) 
+
+ModelHistory <- model_4_16 %>% fit(trainPred[trainIndex[ ,i], ], trainResponse[trainIndex[, i]], epochs = 100, batch_size = b, validation_data = list(trainPred[-trainIndex[, i], ], trainResponse[-trainIndex[ ,i]]), callbacks = callbacks)
+his <- rbind(his, ModelHistory$metrics) 
+
+ModelHistory <- model_4_32 %>% fit(trainPred[trainIndex[ ,i], ], trainResponse[trainIndex[, i]], epochs = 100, batch_size = b, validation_data = list(trainPred[-trainIndex[, i], ], trainResponse[-trainIndex[ ,i]]), callbacks = callbacks)
+his <- rbind(his, ModelHistory$metrics) 
+
+ModelHistory <- model_8_4 %>% fit(trainPred[trainIndex[ ,i], ], trainResponse[trainIndex[, i]], epochs = 100, batch_size = b, validation_data = list(trainPred[-trainIndex[, i], ], trainResponse[-trainIndex[ ,i]]), callbacks = callbacks)
+his <- rbind(his, ModelHistory$metrics) 
+
+ModelHistory <- model_8_8 %>% fit(trainPred[trainIndex[ ,i], ], trainResponse[trainIndex[, i]], epochs = 100, batch_size = b, validation_data = list(trainPred[-trainIndex[, i], ], trainResponse[-trainIndex[ ,i]]), callbacks = callbacks)
+his <- rbind(his, ModelHistory$metrics) 
+
+ModelHistory <- model_8_16 %>% fit(trainPred[trainIndex[ ,i], ], trainResponse[trainIndex[, i]], epochs = 100, batch_size = b, validation_data = list(trainPred[-trainIndex[, i], ], trainResponse[-trainIndex[ ,i]]), callbacks = callbacks)
+his <- rbind(his, ModelHistory$metrics) 
+
+ModelHistory <- model_8_32 %>% fit(trainPred[trainIndex[ ,i], ], trainResponse[trainIndex[, i]], epochs = 100, batch_size = b, validation_data = list(trainPred[-trainIndex[, i], ], trainResponse[-trainIndex[ ,i]]), callbacks = callbacks)
+his <- rbind(his, ModelHistory$metrics) 
+
+ModelHistory <- model_16_4 %>% fit(trainPred[trainIndex[ ,i], ], trainResponse[trainIndex[, i]], epochs = 100, batch_size = b, validation_data = list(trainPred[-trainIndex[, i], ], trainResponse[-trainIndex[ ,i]]), callbacks = callbacks)
+his <- rbind(his, ModelHistory$metrics) 
+
+ModelHistory <- model_16_4 %>% fit(trainPred[trainIndex[ ,i], ], trainResponse[trainIndex[, i]], epochs = 100, batch_size = b, validation_data = list(trainPred[-trainIndex[, i], ], trainResponse[-trainIndex[ ,i]]), callbacks = callbacks)
+his <- rbind(his, ModelHistory$metrics) 
+
+ModelHistory <- model_16_4 %>% fit(trainPred[trainIndex[ ,i], ], trainResponse[trainIndex[, i]], epochs = 100, batch_size = b, validation_data = list(trainPred[-trainIndex[, i], ], trainResponse[-trainIndex[ ,i]]), callbacks = callbacks)
+his <- rbind(his, ModelHistory$metrics) 
+
+ModelHistory <- model_16_4 %>% fit(trainPred[trainIndex[ ,i], ], trainResponse[trainIndex[, i]], epochs = 100, batch_size = b, validation_data = list(trainPred[-trainIndex[, i], ], trainResponse[-trainIndex[ ,i]]), callbacks = callbacks)
+his <- rbind(his, ModelHistory$metrics) 
+
+}
+}
+finishtime <- Sys.time()
 PlotData <- data.frame(x = c(5:ModelHistory$params$epochs), y = ModelHistory$metrics$val_mae [-c(1:4)])
 ggplot(PlotData, aes(x = x, y = y)) + geom_smooth() + xlab("Epoch") + ylab("Estimated Validation MAE loss")
 
 
-model %>% fit(trainPred, trainResponse, epochs = 125, batch_size = 8, validation_data = list(testPred, testResponse))
+model %>% fit(trainPred, trainResponse, epochs = 10, batch_size = 8, validation_data = list(testPred, testResponse))
 
 results <- model %>% evaluate(testPred, testResponse)
 
-results$Predictions <- model %>% predict(testPred)
-trainAss$Predictions <- model %>% predict(trainPred)
+results$Predictions <- model %>% predict(testPred) %>% as.numeric()
+trainAss$Predictions <- model %>% predict(trainPred) %>% as.numeric()
 
+results <- as.data.frame(results)
 ggplot(data = results) + geom_line(aes(x = Date, y = Predictions), colour = "red") +geom_line(aes(x = Date, y = Y), colour = "blue") 
 ggplot(data = trainAss) + geom_line(aes(x = Date, y = Predictions), colour = "red") +geom_line(aes(x = Date, y = Y), colour = "blue") 
+ 
